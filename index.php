7807a89cbbba565645ea9fd0d006a3996f4a41e1
KwertyMU <46666242+KwertyMU@users.noreply.github.com>
2019-01-15 11:57:45 +0700

    добавил index фаил проекта

---
+++ index.php
@@ -0,0 +1,484 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>My Portfolio</title>
+
+	<!-- CSS -->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/simple-line-icons.css" rel="stylesheet" media="screen">
+	<link href="assets/css/animate.css" rel="stylesheet">
+    
+	<!-- Custom styles CSS -->
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+    
+    <script src="assets/js/modernizr.custom.js"></script>
+       
+</head>
+<body>
+
+	<!-- Preloader -->
+
+	<div id="preloader">
+		<div id="status"></div>
+	</div>
+
+	<!-- Home start -->
+
+	<section id="home" class="pfblock-image screen-height">
+        <div class="home-overlay"></div>
+		<div class="intro">
+			<div class="start">Здравствуйте, меня зовут Дмитрий Семиренко и я</div>
+			<h1>фронтэнд разработчик</h1>
+			<div class="start">занимаюсь созданием современного и адаптивного веб-приложения</div>
+			<ul class="social">
+               <li><a href="https://vk.com/id264303903" target="_blank"><i class="fa fa-vk"></i></a></li>
+               <li><a href="https://twitter.com/9YcHi7PuI0UNWjS" target="_blank"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="https://plus.google.com/101442886201822100211" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
+               <li><a href="#"><i class="fa fa-envelope-o"></i></a></li>
+            </ul>
+		</div>
+
+        <a href="#services">
+		<div class="scroll-down">
+            <span>
+                <i class="fa fa-angle-down fa-2x"></i>
+            </span>
+		</div>
+        </a>
+
+	</section>
+
+	<!-- Home end -->
+
+	<!-- Navigation start -->
+
+	<header class="header">
+
+		<nav class="navbar navbar-custom" role="navigation">
+
+			<div class="container">
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#custom-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- <a class="navbar-brand" href="index.html">John Doe</a> -->
+				</div>
+
+				<div class="collapse navbar-collapse" id="custom-collapse" style="width: max-content;margin: auto; text-align: center;">
+					<ul class="nav navbar-nav navbar-right" style="display: flex;">
+						<li><a href="#home">Главная</a></li>
+						<li><a href="#about">Обо мне</a></li>
+                        <li><a href="#portfolio">Работы</a></li>
+                        <li><a href="#skills">Навыки</a></li>
+						<li><a href="#testimonials">Отзывы</a></li>
+						<li><a href="#contact">Контакты</a></li>
+					</ul>
+				</div>
+
+			</div><!-- .container -->
+
+		</nav>
+
+	</header>
+
+	<!-- Navigation end -->
+
+    <!-- Services start -->
+
+
+
+	<!-- Services end -->
+
+	<!-- About Section -->
+<div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Обо мне</h2>
+      <div class="pfblock-line"></div>
+      
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center"><img src="assets/images/about.jpg" class="img-responsive"></div>
+      <div class="col-md-8 col-md-offset-2">
+        <div class="about-text">
+          <p><strong>Всем привет! Меня зовут Дмитрий, я Front-end разработчик. </strong>С тех пор как впервые увидел на экране "Hello world!" понял, что это то чему хочу посвятить всего себя! С тех пор утекло немало строчек кода.</p>
+          <p><strong>Моими ключевыми функциональными обязаностями являются: </strong>
+			Верстка новых страниц из PSD макетов
+			Адаптация новых и готовых страниц под мобильные устройства
+			Реализация JavaScript логики (слайдеры, попапы, анимации, AJAX и т.д.)
+			Интеграция новых модулей в готовые проекты</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    
+	<!-- Portfolio start -->
+
+	<section id="portfolio" class="pfblock">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-sm-6 col-sm-offset-3">
+
+					<div class="pfblock-header wow fadeInUp">
+						<h2 class="pfblock-title">Мои работы</h2>
+						<div class="pfblock-line"></div>
+						<div class="pfblock-subtitle">
+							Никто не зажигает лампу, чтобы спрятать ее за дверью: цель света-создать больше света, открыть людям глаза, раскрыть чудеса вокруг.
+						</div>
+					</div>
+
+				</div>
+
+			</div><!-- .row -->
+            
+            
+            <div class="row">
+                
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    
+                    <div class="grid wow zoomIn">
+                    
+                        <figure class="effect-bubba">
+                            <!-- <img src="assets/images/sm.jpg" alt="img01"/> -->
+                            <div class="img" style="background-image: url(assets/images/sm.jpg);  "></div>
+                            <figcaption>
+                                <h2><span>S&Mitler</span></h2>
+                                <p><a href="http://mysmitler.ru/" target="_blank"><button>Подробнее</button></a></p>
+                            </figcaption>			
+                        </figure>
+                    
+                    </div>
+                    
+                </div>
+
+                <div class="col-xs-12 col-sm-4 col-md-4">
+            
+                    <div class="grid wow zoomIn">
+                        <figure class="effect-bubba">
+                            <div class="img" style="background-image: url(assets/images/ds.png);"></div>
+                            <figcaption>
+                                <h2><span>DStransport</span></h2>
+                                <p><a href="http://dstransport.ru"  target="_blank"><button>Подробнее</button></a></p>
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    
+                </div>
+                
+                <div class="col-xs-12 col-sm-4 col-md-4">
+            
+                    <div class="grid wow zoomIn">
+                        <figure class="effect-bubba">
+                            <div class="img" style="background-image: url(assets/images/laib.png);"></div>
+                            <figcaption>
+                                <h2>The <span>Hat</span></h2>
+                                <p><a href=""><button>Подробнее</button></a></p>
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    
+                </div>
+                
+                <div class="col-xs-12 col-sm-4 col-md-4">
+            
+                    <div class="grid wow zoomIn">
+                        <figure class="effect-bubba">
+                            <div class="img" style="background-image: url(assets/images/tat.png);"></div>
+                            <figcaption>
+                                <h2>Bang <span>Bang</span></h2>
+                                <p><a href=""><button>Подробнее</button></a></p>
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    
+                </div>
+                
+                <div class="col-xs-12 col-sm-4 col-md-4">
+            
+                    <div class="grid wow zoomIn">
+                        <figure class="effect-bubba">
+                            <div class="img" style="background-image: url(assets/images/card.png);"></div>
+                            <figcaption>
+                                <h2><span>good cardboard</span></h2>
+                                <p><a href="http://www.semirenko.ru/" target="_blank"><button>Подробнее</button></a></p>
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    
+                </div>
+                
+                <div class="col-xs-12 col-sm-4 col-md-4">
+            
+                    <div class="grid wow zoomIn">
+                        <figure class="effect-bubba">
+                            <div class="img" style="background-image: url(assets/images/mag.png);"></div>
+                            <figcaption>
+                                <h2>Don't <span>Poke</span></h2>
+                                <p><a href=""><button>Подробнее</button></a></p>
+                            </figcaption>			
+                        </figure>
+                    </div>
+                    
+                </div>
+                
+            </div>
+
+
+		</div><!-- .contaier -->
+
+	</section>
+
+	<!-- Portfolio end -->
+    
+    <!-- Skills start -->
+    
+    <section class="pfblock pfblock-gray" id="skills">
+		
+			<div class="container">
+			
+				<div class="row skills">
+					
+					<div class="row">
+
+                        <div class="col-sm-6 col-sm-offset-3">
+
+                            <div class="pfblock-header wow fadeInUp">
+                                <h2 class="pfblock-title">Мои навыки</h2>
+                                <div class="pfblock-line"></div>
+                                <div class="pfblock-subtitle">
+                                    Мне нравится превращать сложные проблемы в простые, красивые и интуитивные проекты. Так же я очень неплохо готовлю, люблю кофе и не бегаю по утрам.
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div><!-- .row -->
+					
+					<div class="col-sm-6 col-md-3 text-center">
+						<span data-percent="90" class="chart easyPieChart" style="width: 140px; height: 140px; line-height: 140px;">
+                            <span class="percent">90</span>
+                        </span>
+						<h3 class="text-center">html5</h3>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span data-percent="90" class="chart easyPieChart" style="width: 140px; height: 140px; line-height: 140px;">
+                            <span class="percent">90</span>
+                        </span>
+						<h3 class="text-center">css/sass</h3>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span data-percent="75" class="chart easyPieChart" style="width: 140px; height: 140px; line-height: 140px;">
+                            <span class="percent">75</span>
+                        </span>
+						<h3 class="text-center">js/JQUERY</h3>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span data-percent="90" class="chart easyPieChart" style="width: 140px; height: 140px; line-height: 140px;">
+                            <span class="percent">90</span>
+                        </span>
+						<h3 class="text-center">gulp</h3>
+					</div>
+					
+				</div><!--End row -->
+			
+			</div>
+		
+    </section>
+    
+    <!-- Skills end -->
+
+	<!-- CallToAction start -->
+
+	<section class="calltoaction">
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-12 col-lg-12">
+					<h2 class="wow slideInRight" data-wow-delay=".1s">Готовы ли вы начать?</h2>
+					<div class="calltoaction-decription wow slideInRight" data-wow-delay=".2s">
+						Я доступен для фриланс-проектов.
+					</div>
+				</div>
+
+				<div class="col-md-12 col-lg-12 calltoaction-btn wow slideInRight" data-wow-delay=".3s">
+					<a href="#contact" class="btn btn-lg">написать мне</a>
+				</div>
+
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</section>
+
+	<!-- CallToAction end -->
+
+	<!-- Testimonials start -->
+
+	<section id="testimonials" class="pfblock pfblock-gray">
+
+		<div class="container">
+            
+            <div class="row">
+
+				<div class="col-sm-6 col-sm-offset-3">
+
+					<div class="pfblock-header wow fadeInUp">
+						<h2 class="pfblock-title">что говорят мои клиенты</h2>
+						<div class="pfblock-line"></div>
+						<div class="pfblock-subtitle">
+							Ключ к успеху – это создание у клиентов реалистичных ожиданий, а затем эти ожидания нужно не просто оправдать, их нужно превзойти.
+						</div>
+					</div>
+
+				</div>
+
+			</div><!-- .row -->
+
+            <div class="row">
+
+			<div id="cbp-qtrotator" class="cbp-qtrotator">
+                <div class="cbp-qtcontent">
+                    <img src="assets/images/client-1.jpg" alt="client-1" />
+                    <blockquote>
+                      <p>Работать с Дмитрием было одно удовольствие. Он точно понял, чего я хочу, и создал потрясающий сайт для моей компании.</p>
+                      <footer>Это мой отзыв</footer>
+                    </blockquote>
+                </div>
+                <div class="cbp-qtcontent">
+                    <img src="assets/images/client-2.jpg" alt="client-2" />
+                    <blockquote>
+                      <p>Я очень довольна результатами. Получить 100% удовлетворение сложно, но Дмитрию удалось этого добиться.</p>
+                      <footer>Это тоже мой отзыв</footer>
+                    </blockquote>
+                </div>
+
+            </div>		
+
+            </div><!-- .row -->
+
+					
+		</div><!-- .row -->
+	</section>
+
+	<!-- Testimonial end -->
+
+
+	<!-- Contact start -->
+
+	<section id="contact" class="pfblock">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-sm-6 col-sm-offset-3">
+
+					<div class="pfblock-header">
+						<h2 class="pfblock-title">Напишите мне</h2>
+						<div class="pfblock-line"></div>
+						<div class="pfblock-subtitle">
+							Если у вас остались какие-либо вопросы просто заполните форму и я отвечу в ближайшее время!
+						</div>
+					</div>
+
+				</div>
+
+			</div><!-- .row -->
+
+			<div class="row">
+
+				<div class="col-sm-6 col-sm-offset-3">
+					<form id="contact-form" role="form">
+						<div class="ajax-hidden">
+							<div class="form-group wow fadeInUp">
+								<label class="sr-only" for="c_name">Name</label>
+								<input type="text" id="c_name" class="form-control" name="c_name" placeholder="Name">
+							</div>
+
+							<div class="form-group wow fadeInUp" data-wow-delay=".1s">
+								<label class="sr-only" for="c_email">Email</label>
+								<input type="email" id="c_email" class="form-control" name="c_email" placeholder="E-mail">
+							</div>
+
+							<div class="form-group wow fadeInUp" data-wow-delay=".2s">
+								<textarea class="form-control" id="c_message" name="c_message" rows="7" placeholder="Message"></textarea>
+							</div>
+
+							<button type="submit" class="btn btn-lg btn-block wow fadeInUp" data-wow-delay=".3s">Send Message</button>
+						</div>
+						<div class="ajax-response"></div>
+					</form>
+				</div>
+
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</section>
+
+	<!-- Contact end -->
+
+	<!-- Footer start -->
+
+	<footer id="footer">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-sm-12">
+
+					<ul class="social-links">
+						<li><a href="index.html#" class="wow fadeInUp"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="index.html#" class="wow fadeInUp" data-wow-delay=".1s"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="index.html#" class="wow fadeInUp" data-wow-delay=".2s"><i class="fa fa-google-plus"></i></a></li>
+						<li><a href="index.html#" class="wow fadeInUp" data-wow-delay=".4s"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="index.html#" class="wow fadeInUp" data-wow-delay=".5s"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+
+					<p class="heart">
+                        Сделано <span class="fa fa-heart fa-2x animated pulse"></span> в Новосибирске
+                    </p>
+                    <p class="copyright">
+                        © 2019 Д.Семиренко | Images: <a href="https://unsplash.com/">Unsplash</a> & <a href="http://zoomwalls.com/">Zoomwalls</a>
+					</p>
+
+				</div>
+
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</footer>
+
+	<!-- Footer end -->
+
+	<!-- Scroll to top -->
+
+	<div class="scroll-up">
+		<a href="#home"><i class="fa fa-angle-up"></i></a>
+	</div>
+    
+    <!-- Scroll to top end-->
+
+	<!-- Javascript files -->
+
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.parallax-1.1.3.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.js"></script>
+	<script src="assets/js/jquery.sticky.js"></script>
+	<script src="assets/js/smoothscroll.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/jquery.easypiechart.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.cbpQTRotator.js"></script>
+	<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
